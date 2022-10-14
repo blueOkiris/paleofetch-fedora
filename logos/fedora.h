fec8e3cc85f223f0b9c14c6a9cd9c5d43c0109cd
Dylan Turner <dylantdmt@gmail.com>
2022-10-14 09:09:42 -0500

    fedora.h: updated to match current neofetch logo

--- logos/fedora.h
+++ logos/fedora.h
@@ -1,6 +1,8 @@
 #define BLU   "\x1B[34m"
 #define WHT   "\x1B[37m"
 
+/*
+ * Original
 char *LOGO[] = {
     BLU"          /:-------------:\\         ",
     BLU"       :-------------------::       ",
@@ -20,3 +22,51 @@ char *LOGO[] = {
     BLU":------"WHT"::SS:"BLU"-------------::         ",
     BLU":---------------------::/           "
 };
+ */
+
+/*
+ * From neofetch:
+ * ${c1}             .',;::::;,'.
+                 .';:cccccccccccc:;,.
+              .;cccccccccccccccccccccc;.
+            .:cccccccccccccccccccccccccc:.
+          .;ccccccccccccc;${c2}.:dddl:.${c1};ccccccc;.
+         .:ccccccccccccc;${c2}OWMKOOXMWd${c1};ccccccc:.
+        .:ccccccccccccc;${c2}KMMc${c1};cc;${c2}xMMc${c1};ccccccc:.
+        ,cccccccccccccc;${c2}MMM.${c1};cc;${c2};WW:${c1};cccccccc,
+        :cccccccccccccc;${c2}MMM.${c1};cccccccccccccccc:
+        :ccccccc;${c2}oxOOOo${c1};${c2}MMM0OOk.${c1};cccccccccccc:
+        cccccc;${c2}0MMKxdd:${c1};${c2}MMMkddc.${c1};cccccccccccc;
+        ccccc;${c2}XM0'${c1};cccc;${c2}MMM.${c1};cccccccccccccccc'
+        ccccc;${c2}MMo${c1};ccccc;${c2}MMW.${c1};ccccccccccccccc;
+        ccccc;${c2}0MNc.${c1}ccc${c2}.xMMd${c1};ccccccccccccccc;
+        cccccc;${c2}dNMWXXXWM0:${c1};cccccccccccccc:,
+        cccccccc;${c2}.:odl:.${c1};cccccccccccccc:,.
+        :cccccccccccccccccccccccccccc:'.
+        .:cccccccccccccccccccccc:;,..
+          '::cccccccccccccc::;,.
+ */
+
+// New:
+char *LOGO[] = {
+    BLU"             .',;::::;,'.               ",
+    BLU"         .';:cccccccccccc:;,.           ",
+    BLU"      .;cccccccccccccccccccccc;.        ",
+    BLU"    .:cccccccccccccccccccccccccc:.      ",
+    BLU"  .;ccccccccccccc;"WHT".:dddl:."BLU";ccccccc;.    ",
+    BLU" .:ccccccccccccc;"WHT"OWMKOOXMWd"BLU";ccccccc:.   ",
+    BLU".:ccccccccccccc;"WHT"KMMc"BLU";cc;"WHT"xMMc"BLU";ccccccc:.  ",
+    BLU",cccccccccccccc;"WHT"MMM."BLU";cc;"WHT";WW:"BLU";cccccccc,",
+    BLU":cccccccccccccc;"WHT"MMM."BLU";cccccccccccccccc:  ",
+    BLU":ccccccc;"WHT"oxOOOo"BLU";"WHT"MMM0OOk."BLU";cccccccccccc:  ",
+    BLU"cccccc;"WHT"0MMKxdd:"BLU";"WHT"MMMkddc."BLU";cccccccccccc;  ",
+    BLU"ccccc;"WHT"XM0'"BLU";cccc;"WHT"MMM."BLU";cccccccccccccccc'  ",
+    BLU"ccccc;"WHT"MMo"BLU";ccccc;"WHT"MMW."BLU";ccccccccccccccc;   ",
+    BLU"ccccc;"WHT"0MNc."BLU"ccc"WHT".xMMd"BLU";ccccccccccccccc;    ",
+    BLU"cccccc;"WHT"dNMWXXXWM0:"BLU";cccccccccccccc:,     ",
+    BLU"cccccccc;"WHT".:odl:."BLU";cccccccccccccc:,.      ",
+    BLU":cccccccccccccccccccccccccccc:'.        ",
+    BLU".:cccccccccccccccccccccc:;,..           ",
+    BLU"  '::cccccccccccccc::;,.                "
+};
+
